e3743d9684fa13957004b69102431343cde92a5c
soniabergamini <sounia96@outlook.it>
2023-07-17 17:33:42 +0200

    feat(api): add paginate

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::all();
+        $projects = Project::with('type', 'technologies')->paginate(3);
         $response = [
             "success" => true,
             "results" => $projects,
