6ef11fe68b15f58470f97d1565c905ff5931af96
soniabergamini <sounia96@outlook.it>
2023-07-17 15:13:28 +0200

    perf: rename api route

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/projectss", [ProjectController::class, "index"]);
+Route::get("/projects", [ProjectController::class, "index"]);
