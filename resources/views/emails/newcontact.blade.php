7cbb83fe976cc9ffd2c852a8e32d508b0c87bbbd
soniabergamini <sounia96@outlook.it>
2023-07-21 17:25:45 +0200

    style(mail): improve template

--- resources/views/emails/newcontact.blade.php
+++ resources/views/emails/newcontact.blade.php
@@ -2,28 +2,31 @@
 
 <hr>
 
-<p>
-    You have received a <strong>new contact request</strong>. Here is the user information and message for you: <br>
-    <ul>
-        <li>
-            <span><strong>Name: </strong>{{ $lead['name'] }}</span>
-        </li>
-        <li>
-            <span><strong>Email: </strong>{{ $lead['email'] }}</span>
-        </li>
-        <li>
-            <span><strong>Message: </strong>{{ $lead['message'] }}</span>
-        </li>
-    </ul>
-</p>
+<p>You have received a <strong>new contact request</strong>. Here is the user information and message for you: </p>
+
+<ul>
+    <li>
+        <span><strong>Name: </strong>{{ $lead['name'] }}</span>
+    </li>
+    <li>
+        <span><strong>Email: </strong>{{ $lead['email'] }}</span>
+    </li>
+    <li>
+        <span><strong>Message: </strong>{{ $lead['message'] }}</span>
+    </li>
+</ul>
+
 
 <hr>
 
 <style>
-    h1, p {
+    h1, p, li {
         font-family: Verdana, Geneva, Tahoma, sans-serif
     }
     h1 {
         text-align: center
+    }
+    li {
+        margin: 1rem 0;
     }
 </style>
